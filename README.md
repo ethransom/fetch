bf16f3d0d07d552a576831618a53fcd2357cd18b
Ethan Ransom <derethanhausen@gmail.com>
2012-03-13 13:37:21 -0600

    Git the code in the repo. :)

---
+++ README.md
@@ -0,0 +1,60 @@
+#Fetch - A simple asset preloader
+
+----------
+
+Loading of assets with fetch is done with `loader` functions. Fetch has two loaders included: 
+
+- `script` - Used for loading scripts
+- `image` - Used for loading images
+
+Writing your own loaders enables you to load just about anything, in any way you want to:
+
+	Fetch.loader('sound',function (url, callback) {
+		// We'll see where the url param comes from in a moment
+		var a = new Audio();
+		a.src = url;
+
+		// The callback param is a function that should 
+		// be called when this particullar asset is loaded
+		a.addEventListener('canplaythrough', callback, false);
+
+		// Returning the asset will allow us to use it later
+		return a;
+	});
+
+The loading of assets is triggered thusly:
+
+	Fetch.load({
+		'sound': [	// name of the loader to use
+			'sounds/bark.ogg',	// path to the asset
+			'sounds/meow.ogg'	// These are passed to the loader
+		]
+	});
+
+Once assets have been loaded, they will need to be worked with. Accessing assets is as easy as:
+
+	Fetch.asset('type','original/asset/path');
+
+If we were to use the sound example, our code would look like this:
+
+	Fetch.asset('sound','sounds/bark.ogg');
+	// Returns an Audio object, ready to be played
+
+You will probably want to display some sort of progress to the user (think GMail). 
+
+	Fetch.on('update', function ( progress ) {
+		// progress is a percent value, eg: .59 for 59%
+		$('#progress').width( progress * 500 );
+	});
+
+You can be notified when all the assets are done loading and ready to be used.
+
+	Fetch.on('done', function () {
+		// Launch your code here
+	});
+
+Check out `demo.html` for a complete example.
+
+----------
+
+> Copyright (c) 2012 Ethan Ransom. MIT Licensed.
---
+++ fetch.js
@@ -0,0 +1,127 @@
+/*
+ * Fetch - A simple asset preloader
+ * v1.0 - 3/13/2012
+ * Copyright (c) 2012 Ethan Ransom
+ * derethanhausen [at] gmail.com
+ * Licensed under the MIT license
+*/
+
+var Fetch = (function () {
+	// used to keep track of loading progress
+	var toLoad = 0;
+	var haveLoaded = 0;
+
+	var assets = {};
+	
+	var loaders = {};
+	
+	// mini internal event system
+	var subscribers = {};
+	var on = function ( event, callback ) {
+		// Subcribes callbacks to events
+		if( !subscribers[event] )
+			subscribers[event] = [];
+
+		subscribers[event].push( callback );
+	};
+	var trigger = function ( event, arg ) {
+		// Private. Used internally to trigger callbacks.
+		if( !subscribers[event] )
+			return;
+			
+		for( var i = 0; i < subscribers[event].length; i++ ) {
+			subscribers[ event ][ i ]( arg );
+		}
+	};
+	
+	var updateLoader = function () {
+		// Does the math necessary to update loader state. 
+		// Calls the update callback to (presumably) update the GUI.
+		// If the loading is complete, call the done callback.
+		var percent = (haveLoaded / toLoad);
+
+		trigger('update', percent );
+		
+		if( percent >= 1 )
+			trigger('done');
+	};
+	
+	var callback = function () {
+		// called by loader functions when an asset is loaded
+		haveLoaded++;
+		updateLoader();
+	}
+	
+	var load = function (list) {
+		// Triggers the loading of assets using pre-defined "loader" functions.
+		// See the README for example usage
+		for( type in list ) {
+			assets[type] = {};
+			if( loaders[type] ) {
+					// each asset type has many sub assets, load each one using loader function
+					for( var i = 0; i < list[type].length; i++ ) {
+						assets[type][ list[type][i] ] = loaders[type]( list[type][i], callback );
+						toLoad++;
+					}
+			} else {
+				throw new Error("No loader for type: " + type );
+			}
+		}
+		
+		updateLoader();
+	};
+	
+	var loader = function (name, loader) {
+		// defines a loader
+		loaders[name] = loader;
+	};
+	
+	var asset = function ( type, name ) {
+		// Returns an asset. If the asset is not found, and there is a corresponding loader, it will load a "fresh" copy of the asset.
+		a = false;
+		try {
+			// attempt to fetch the image from cache
+			a = assets[type][name];
+			if( !a )
+				throw "Asset not in cache!";
+		} catch (e) {
+			// try to load a fresh one, or just die
+			if( !loaders[type] )
+				throw new Error("No loader for type: " + type );
+			a = loaders[type]( name, function () {} );
+			console.log( e + ' Loaded fresh: ' + name );
+		}
+		return a;
+	};
+	
+	// expose only public methods and vars
+	return {
+		'load': load,
+		'loader': loader,
+		'asset': asset,
+		'on': on
+	};
+})();
+
+// Pre-implemented loader functions for commonly used asset types (images and scripts)
+
+Fetch.loader('image',function ( url, callback ) {
+	var img = new Image();
+	img.onload = callback;
+	img.src = url;
+	return img;
+});
+
+Fetch.loader('script', function ( url, callback ) {
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.onreadystatechange = function () {
+		if (this.readyState === 'complete') 
+			callback();
+	}
+	script.onload = callback;
+	script.src = url;
+	head.appendChild(script);
+	return true;
+});
