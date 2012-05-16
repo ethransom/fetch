6109f4306874e5785d7913cc0b0920691906f859
Ethan Ransom <derethanhausen@gmail.com>
2012-05-16 17:48:29 -0600

    Add loader tests

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -2,4 +2,16 @@ describe('Fetch', function(){
 	it('has a version number', function(){
 		expect( Fetch.version ).to.match(/[0-9]+\.[0-9]+/);
 	});
+	
+	describe('#loader', function(){
+		it('must be exposed', function(){
+			expect(Fetch.loader).to.be.an('function');
+		});
+		
+		it('must create a functional loader', function(){
+			Fetch.loader('test', function(string, callback){
+				callback();
+			});
+		});
+	});
 });
